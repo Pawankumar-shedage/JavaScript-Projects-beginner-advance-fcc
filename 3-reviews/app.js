60e426efca712cccd4cb73833798a736c92a8fbb
Pawankumar Shedage <pawankumarshedage@gmail.com>
2023-08-19 19:23:02 +0530

    started reviews project, generate reviews

---
+++ 3-reviews/app.js
@@ -0,0 +1,11 @@
+
+const review =[
+    {
+        id:1,
+        name:"Pawankumar Shedage",
+        job:"Front-End Developer",
+        img:"http://
+        text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+
+    }
+]
